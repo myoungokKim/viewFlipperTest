fb2b411672d4d4f1b4535ec6e33ced2bfc1c59f6
Myoungok Kim <cheese0414@naver.com>
2016-05-24 20:13:23 +0900

    #2 Event Programming

--- app/src/main/java/kr/hs/emirim/cheese0414/viewflippertest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/cheese0414/viewflippertest/MainActivity.java
@@ -1,13 +1,39 @@
 package kr.hs.emirim.cheese0414.viewflippertest;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ViewFlipper viewFlip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button butPrev=(Button)findViewById(R.id.but_prev);
+        Button butNext=(Button)findViewById(R.id.but_next);
+        butPrev.setOnClickListener(this);
+        butNext.setOnClickListener(this);
+        viewFlip=(ViewFlipper)findViewById(R.id.view_flip);
+        viewFlip.setOnTouchListener(this);
     }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.but_prev:
+                viewFlip.showPrevious();
+                break;
+            case R.id.but_next:
+                viewFlip.showNext();
+                break;
+        }
+    }
+
 }
